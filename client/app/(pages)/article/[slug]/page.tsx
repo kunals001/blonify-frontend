9ceee719b55b60f42d779c42c286b92bbb51e146
kunals001 <kunaltrad555@gmail.com>
2025-05-24 12:31:30 +0530

    update

--- client/app/(pages)/article/[slug]/page.tsx
+++ client/app/(pages)/article/[slug]/page.tsx
@@ -4,8 +4,10 @@ import Head from 'next/head';
 import { useParams } from 'next/navigation';
 import LoadingSpinner from '@/components/LoadingSpinner';
 import axios from 'axios';
+import DailyPostPage from "@/components/Daily/DailyPostPage"
+import MobilePostPage from "@/components/Mobile/MobilePostPage"
+import LaptopPostPage from "@/components/Laptop/LaptopPostPage"
 import "../../../globals.css"
-import dynamic from 'next/dynamic';
 
 
 export type Post ={
@@ -139,19 +141,6 @@ export type Post ={
   }[];
   userId?: string;
 }
-
-
-const DailyPostPage = dynamic(() => import('@/components/Daily/DailyPostPage'), {
-  ssr: false, // Isse ye component sirf client side pe load hoga
-});
-
-const MobilePostPage = dynamic(() => import('@/components/Mobile/MobilePostPage'), {
-  ssr: false, // Isse ye component sirf client side pe load hoga
-});
-
-const LaptopPostPage = dynamic(() => import('@/components/Laptop/LaptopPostPage'), {
-  ssr: false, // Isse ye component sirf client side pe load hoga
-});
 
 
 const Page = () => {
