9fadd93e28938d3d63691c2221d0f93601e9096f
kunals001 <kunaltrad555@gmail.com>
2025-05-25 21:25:34 +0530

    css update

--- client/components/FeaturedCarsol.tsx
+++ client/components/FeaturedCarsol.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import ImageKit from './Image';
-import Head from 'next/head';
 
 interface Post {
   _id: string;
@@ -15,32 +14,23 @@ const FeaturedCarsol = ({ post }: { post: Post}) => {
 
   return (
     <>
-
-      <Head>
-        <link
-          rel="preload"
-          as="image"
-          href={`${post.coverImg}?tr=w-1200,h-700,fo-auto,f-webp,q-70`}
-        />
-      </Head>
-
-
-    <div className='relative overflow-hidden rounded-xl'>
+    <div className='md:relative w-full overflow-hidden rounded-xl flex flex-row md:block gap-[1vh] md:gap-0 bg-gradient-to-b from-transparent to-green-200'>
         <Link href={`/article/${post?.slug}`}>
             <ImageKit 
-              w={500} 
-              h={400} 
+              w={400} 
+              h={350} 
               src={post?.coverImg}
               alt={post?.altText}
-              className="w-full relative h-[26.5vh] md:h-[30vw] lg:h-[30vw] rounded-xl object-cover overflow-hidden"
+              className="w-[16vh] md:w-full md:relative block h-[11.5vh] md:h-[30vw] rounded-xl object-cover overflow-hidden"
               loading="eager"
-              priority={true}
               />
-            </Link>
+          </Link>
 
-            <div className="absolute bottom-0 w-full md:h-[5vw] h-[6vh] flex gap-[1vh] rounded-b-xl md:px-[1vw] px-[1vh] md:py-[1vw] py-[1vh] items-center bg-gradient-to-b from-transparent to-zinc-300">
-                <h1 className='text-[2vh] md:text-[1.7vw] lg:text-[1.7vw] hover:underline leading-none cursor-pointer font-semibold text-zinc-800'>{post?.title}</h1>
-            </div>
+            <Link href={`/article/${post?.slug}`} ><h1 className='text-[2vh] w-[26.5vh] md:hidden block leading-none font-semibold text-zinc-800 pt-2'>{post?.title}</h1></Link>
+
+           <div className="absolute bottom-0 w-full hidden md:h-[5vw] h-[6vh] md:flex gap-[1vh] rounded-b-xl md:px-[1vw] px-[1vh] md:py-[1vw] py-[1vh] items-center bg-gradient-to-b from-transparent to-zinc-300">
+                <h1 className='text-[2vh] md:text-[1.7vw] hover:underline leading-none cursor-pointer font-semibold text-zinc-800'>{post?.title}</h1>
+          </div>
     </div>
     </>
   )
