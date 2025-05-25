bcc78f81937c8bffe6861c688ada2cb847a62e66
kunals001 <kunaltrad555@gmail.com>
2025-05-25 19:23:43 +0530

    added

--- client/components/FeaturedCarsol.tsx
+++ client/components/FeaturedCarsol.tsx
@@ -15,8 +15,8 @@ const FeaturedCarsol = ({ post }: { post: Post}) => {
     <div className='relative overflow-hidden rounded-xl'>
         <Link href={`/article/${post?.slug}`}>
             <ImageKit 
-              w={600} 
-              h={400} 
+              w={500} 
+              h={350} 
               src={post?.coverImg}
               alt={post?.altText}
               className="w-full relative h-[26.5vh] md:h-[30vw] lg:h-[30vw] rounded-xl object-cover overflow-hidden"
@@ -24,7 +24,7 @@ const FeaturedCarsol = ({ post }: { post: Post}) => {
               />
             </Link>
 
-              <div className="absolute bottom-0 w-full md:h-[5vw] h-[6vh] flex gap-[1vh] rounded-b-xl md:px-[1vw] px-[1vh] md:py-[1vw] py-[1vh] items-center bg-gradient-to-b backdrop-blur-sm from-transparent to-zinc-400">
+              <div className="absolute bottom-0 w-full md:h-[5vw] h-[6vh] flex gap-[1vh] rounded-b-xl md:px-[1vw] px-[1vh] md:py-[1vw] py-[1vh] items-center bg-gradient-to-b from-transparent to-zinc-300">
                 <h1 className='text-[2vh] md:text-[1.7vw] lg:text-[1.7vw] hover:underline leading-none cursor-pointer font-semibold text-zinc-800'>{post?.title}</h1>
             </div>
     </div>
--- client/components/FeaturedPost.tsx
+++ client/components/FeaturedPost.tsx
@@ -6,7 +6,13 @@ import { Skeleton } from './ui/skeleton';
 
 const FeaturedPost = ({posts}: {posts: Post[]}) => {
 
-  
+  let filteredPosts = posts.filter(
+    (post) => post.isFeatured 
+  );
+
+  if (filteredPosts.length > 1) {
+    filteredPosts = filteredPosts.slice(filteredPosts.length - 1);
+  }
 
   return (
     <div className='w-full md:h-[30vw] lg:h-[30vw] py-[1vh] md:py-[.1vw] lg:py-[.1vw] lg:flex md:flex gap-[3vh] md:gap-[.5vw] lg:gap-[.5vw] flex flex-col md:flex-row lg:flex-row relative select-none'>
@@ -14,12 +20,11 @@ const FeaturedPost = ({posts}: {posts: Post[]}) => {
 
         {posts.length > 0 ? (
           <div className="w-full">
-            {posts
-              .filter((post: Post) => post.isFeatured)
+            {filteredPosts
               .map((post: Post, index: number) => (
                 <div className='w-full' key={index}>
                   <FeaturedCarsol post={post} />
-                  </div>
+                </div>
             ))}
             </div>
         ) :(
